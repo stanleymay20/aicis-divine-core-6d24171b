9832f523b2632b049b10f4fd2336669c03c2f813
Stanley Osei-Wusu <107648937+stanleymay20@users.noreply.github.com>
2026-08-22 15:35:19 +0200

    Expose cognitive substrate modules

---
+++ src/lib/cognitive/index.ts
@@ -0,0 +1,7 @@
+export * from "./contracts";
+export * from "./graph";
+export * from "./learning";
+export * from "./orchestrator";
+export * from "./perception";
+export * from "./pipeline";
+export * from "./topology";
