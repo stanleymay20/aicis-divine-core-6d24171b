89d4d32ef96a9cd2943020848e380c4aa9432621
Stanley Osei-Wusu <107648937+stanleymay20@users.noreply.github.com>
2026-08-31 15:32:40 +0200

    Use PKCE for browser authentication flows

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -21,5 +21,6 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABL
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
+    flowType: 'pkce',
   },
 });
